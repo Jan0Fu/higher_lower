5458c0f168dd511e64474e3a614644538554288d
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-12-01 12:25:19 +0100

    Finished main logic of higher-lower game

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
 import random
 from art import logo, vs
 from game_data import data
+import os
 
 def random_profile():
     return (random.choice(data))
 
-def compare():
-    if profile1 == profile2:
-        profile2 = random_profile()
-    if profile 
-
-
-#while not game_over:
 def higher_lower():
     score = 0
-    profile1 = random_profile()
-    profile2 = random_profile()
-    if profile1 == profile2:
+    game_over = False
+    profile1 = 0
+    while not game_over:
+        os.system('clear')
+        print(logo)
+        if profile1 == 0:
+            profile1 = random_profile()
         profile2 = random_profile()
-    print(profile1)
-    print(profile2)
-    input("Pick profile 'A' or profile 'B': ")
-    
+        if profile1 == profile2:
+            profile2 = random_profile()
+        if score == 0:
+            print(f"Current score: {score}")
+        else:
+            print(f"You're right! Current score: {score}")
+        print(f" Compare A: {profile1}")
+        print(vs)
+        print(f" Against B: {profile2}")
+        user_pick = input("Pick profile 'A' or profile 'B': ").lower()
+        if user_pick == 'a' and profile1['follower_count'] > profile2['follower_count']:
+            score += 1
+            profile1 = profile2
+        elif user_pick == 'a' and profile2['follower_count'] > profile1['follower_count']:
+            game_over = True
+        elif user_pick == 'b' and profile2['follower_count'] > profile1['follower_count']:
+            score += 1
+            profile1 = profile2
+        elif user_pick == 'b' and profile2['follower_count'] < profile1['follower_count']:
+            game_over = True
+        else:
+            print("Invalid input")
+        
+higher_lower()
+print("Wrong, Game Over.") #Final score: {score}")
