4dd6e9374a10e5ae007a471d9245ed00fa36c11f
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-12-01 09:57:01 +0100

    Defined core parts of application

---
+++ main.py
@@ -0,0 +1,24 @@
+import random
+from art import logo, vs
+from game_data import data
+
+def random_profile():
+    return (random.choice(data))
+
+def compare():
+    if profile1 == profile2:
+        profile2 = random_profile()
+    if profile 
+
+
+#while not game_over:
+def higher_lower():
+    score = 0
+    profile1 = random_profile()
+    profile2 = random_profile()
+    if profile1 == profile2:
+        profile2 = random_profile()
+    print(profile1)
+    print(profile2)
+    input("Pick profile 'A' or profile 'B': ")
+    
